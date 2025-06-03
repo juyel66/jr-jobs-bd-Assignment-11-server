d0d1c9640371300dad24806b46270cb9799e8202
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-06-04 00:18:14 +0600

    Initial commit for server

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'https://assignment-11-d5563.web.app',
-    'https://jr-job-bd.netlify.app'
+    // 'http://localhost:5173',
+    // 'http://localhost:5174',
+    // 'https://assignment-11-d5563.web.app',
+    // 'https://jr-job-bd.netlify.app',
+    // 'https://jobs-bd.netlify.app/'
+    
   ],
   credentials: true
 }));
